d24863007c76984b1d02360ea9d61eedf2821813
Clark <mcevoyclark@gmail.com>
2024-06-07 14:23:16 +0200

    work in progress

--- cappa-houzze/src/components/Layouts/ContentLayout.tsx
+++ cappa-houzze/src/components/Layouts/ContentLayout.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 
 interface ContentLayoutProps {
-
+  title: string;
+  body: string;
 }
 
-const ContentLayout: React.FC<ContentLayoutProps> = () => {
+const ContentLayout: React.FC<ContentLayoutProps> = ({ title, body }) => {
   return (
     <div>
-      <h1>Content Layout</h1>
-      <p>This is the content layout</p>
+      <h1>{title}</h1>
+      <p>{body}</p>
     </div>
   );
-}
+};
 
 export default ContentLayout;
--- cappa-houzze/src/components/Paintings.tsx
+++ cappa-houzze/src/components/Paintings.tsx
@@ -2,8 +2,8 @@ import { useEffect, useState } from "react";
 // import { paintings } from "./utils";
 import { useMenuAnimation } from "../animation/utils";
 // import Painting from "./Painting";
-import useEmblaCarousel from "embla-carousel-react";
-import { Carrousel } from "./Caroussel";
+
+// import { Carrousel } from "./Caroussel";
 
 function Paintings() {
   // const [isRendered, setIsRendered] = useState<boolean>(false);
@@ -17,21 +17,12 @@ function Paintings() {
 
   return (
     <div ref={null} className="bg-tertiary w-full h-screen overflow-scroll">
+      {/*  Paintings */}
       {/* <div className="grid grid-cols-3">
         {paintings.map((painting, index) => (
           <Painting index={index} data={painting} />
         ))}
       </div> */}
-      {/* <div className="embla" ref={emblaRef}>
-        {" "}
-        <div className="embla__container">
-          {" "}
-          <div className="embla__slide">Slide 1</div>{" "}
-          <div className="embla__slide">Slide 2</div>{" "}
-          <div className="embla__slide">Slide 3</div>{" "}
-        </div>{" "}
-      </div> */}
-      <Carrousel />
     </div>
   );
 }
