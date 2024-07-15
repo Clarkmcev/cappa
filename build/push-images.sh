6c1e6926f5ee7aa3d27cb71e394202ebd29e15b0
Clark <mcevoyclark@gmail.com>
2024-07-15 17:13:45 +0200

    feat: aws CLI wip

--- build/push-images.sh
+++ build/push-images.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-
-echo "-- Pushing backend image:"
+# echo "-- Pushing backend image :"
 # Do some magic
 
-echo "-- Pushing frontend image:"
+STUDIO_AWS="ubuntu@ec2-52-90-128-43.compute-1.amazonaws.com"
 
+echo "-- Pushing frontend image :"
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 254352282618.dkr.ecr.us-east-1.amazonaws.com
+docker tag frontend:latest 254352282618.dkr.ecr.us-east-1.amazonaws.com/frontend:latest
+docker push 254352282618.dkr.ecr.us-east-1.amazonaws.com/frontend:latest
