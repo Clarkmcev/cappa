106e88d4ad29ab6dfd8da036f2e67081e2747101
Clark <mcevoyclark@gmail.com>
2024-07-17 21:48:51 +0200

    fix: work in progress

--- build/push-images.sh
+++ build/push-images.sh
@@ -9,3 +9,5 @@ echo "-- Pushing frontend image :"
 aws ecr get-login-password --region eu-north-1 | docker login --username AWS --password-stdin 254352282618.dkr.ecr.eu-north-1.amazonaws.com
 docker tag cappa-caddy $CONTAINER_AWS
 docker push $CONTAINER_AWS
+
+# sudo scp -i ~/.ssh/studio.pem -r ./cappa ./cappa ubuntu@ec2-52-90-128-43.compute-1.amazonaws.com:/home/ubuntu
