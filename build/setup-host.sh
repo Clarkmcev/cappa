32bf6b541744c03bd4fffe3b14d4a916e41f3dfa
Clark <mcevoyclark@gmail.com>
2024-07-24 22:34:13 +0200

    work in progress

--- build/setup-host.sh
+++ build/setup-host.sh
@@ -6,9 +6,10 @@ scp_key() {
 }
 
 ssh_custom() {
-    echo "Connecting to the Studio EC2 instance : $1"
+    echo "Connecting to the Studio EC2 instance: $1"
     ssh -i "$SECRET_PEM_PATH" "$@"
 }
 
-scp_key ./docker-compose.yml $STUDIO_EC2:/home/ubuntu
+# scp_key ./docker-compose.yml $STUDIO_EC2:/home/ubuntu
+scp_key ./.env $STUDIO_EC2:/home/ubuntu
 # ssh_custom "$STUDIO_EC2"
