19ea03f6ce517ff51733fc78debc1c3d34d6ef24
Clark <mcevoyclark@gmail.com>
2024-07-28 20:19:05 +0200

    work in progress

--- frontend/src/components/Paintings.tsx
+++ frontend/src/components/Paintings.tsx
@@ -4,11 +4,11 @@ import Painting from "./Painting";
 function Paintings() {
   return (
     <div ref={null} className="w-full h-screen overflow-scroll">
-      <div className="grid grid-cols-1 lg:grid-cols-3 xl:grid-cols-4">
+      {/* <div className="grid grid-cols-1 lg:grid-cols-3 xl:grid-cols-4">
         {paintings.map((painting, index) => (
           <Painting index={index} data={painting} />
         ))}
-      </div>
+      </div> */}
     </div>
   );
 }
