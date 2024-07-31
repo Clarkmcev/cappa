4e77f06aa63b23c6e33ede267145f366508f51f6
Clark <mcevoyclark@gmail.com>
2024-07-31 16:29:17 +0200

    work in progress

--- .bash_custom.sh
+++ .bash_custom.sh
@@ -1,5 +1,9 @@
+
+# ssh connection
 studio() {
-  echo "Connecting via ssh to $STUDIO_AWS"
+  echo "Connecting via ssh to $STUDIO_AWS, secret $SECRET_PEM_PATH, $STUDIO_EC2"
   ssh -i $SECRET_PEM_PATH "$STUDIO_EC2"
 }
 
+# sourcing
+alias src-studio='. ~/code/cappa/.env'
