a8b6b4859c95367b99fab9d7742478643b1734d9
Clark <mcevoyclark@gmail.com>
2024-09-20 16:40:05 +0200

    work in progress

--- build/setup-host.sh
+++ build/setup-host.sh
@@ -13,16 +13,19 @@ ssh_custom() {
 }
 
 # Copy the necessary files to the Studio EC2 instance
-scp_key ./docker-compose.yml $STUDIO_EC2:/home/ubuntu
+# scp_key ./docker-compose.yml $STUDIO_EC2:/home/ubuntu
 scp_key ./.env $STUDIO_EC2:/home/ubuntu
-scp_key ./Caddyfile $STUDIO_EC2:/home/ubuntu
+# scp_key ./Caddyfile $STUDIO_EC2:/home/ubuntu
 
 # Removing the docker images
-echo "Removing docker images ..."
-ssh_custom docker rmi $(docker images -q)
+# echo "Removing docker images ..."
+# ssh_custom docker rmi $(docker images -q)
 
 # Pulling images from AWS ECR
-echo "Pulling images from AWS ECR ..."
+# echo "Pulling images from AWS ECR ..."
+# ssh_custom aws configure
+
+# ssh_custom aws --region eu-north-1 ecr get-login-password 
 ssh_custom aws --region eu-north-1 ecr get-login-password | docker login --username AWS --password-stdin 254352282618.dkr.ecr.eu-north-1.amazonaws.com/amanda-studio
-ssh_custom docker pull 254352282618.dkr.ecr.eu-north-1.amazonaws.com/amanda-studio:latest
+# ssh_custom sudo docker pull 254352282618.dkr.ecr.eu-north-1.amazonaws.com/amanda-studio:latest
 # docker tag 254352282618.dkr.ecr.eu-north-1.amazonaws.com/amanda-studio:latest $STUDIO_FRONTEND_IMAGE
