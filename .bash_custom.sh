3336cd259347cb01f88b3cc766ce40066b3ff88f
Clark <mcevoyclark@gmail.com>
2024-09-25 16:23:43 +0200

    fix: placed back bash

---
+++ .bash_custom.sh
@@ -0,0 +1,7 @@
+# ssh connection
+studio() {
+  echo "Connecting via ssh to $STUDIO_AWS, secret $SECRET_PEM_PATH, $STUDIO_EC2"
+  ssh -i $SECRET_PEM_PATH "$STUDIO_EC2"
+}
+# sourcing
+alias src-studio='. ~/code/cappa/.env'
