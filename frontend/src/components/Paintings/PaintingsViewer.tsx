c228214c1cd9fdeef0fff187ed55c1c3bbf1ca82
Clark <mcevoyclark@gmail.com>
2024-09-29 18:36:44 +0200

    feat: home button painting viewer

--- frontend/src/components/Paintings/PaintingsViewer.tsx
+++ frontend/src/components/Paintings/PaintingsViewer.tsx
@@ -6,16 +6,18 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
-import { VARIANT_PAINTING_VIEWER } from "../../constants";
+import { ROUTE_PAINTINGS, VARIANT_PAINTING_VIEWER } from "../../constants";
 import Background from "../Background";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 function PaintingsViewer() {
   const { id } = useParams();
   const idNumber = Number(id);
+  const navigate = useNavigate();
 
   return (
-    <main className="absolute w-screen h-screen z-50 bg-black/90">
+    <main className="w-screen relative h-screen z-50 bg-black/90">
       <Swiper
         initialSlide={idNumber - 1}
         className="h-screen w-full"
@@ -40,6 +42,15 @@ function PaintingsViewer() {
           </SwiperSlide>
         ))}
       </Swiper>
+      <div className="lowercase absolute top-4 right-4 z-50 group p-4 w-44 duration-150 transition-all">
+        <button
+          className="absolute top-4 right-4 z-50 flex space-x-2 items-center text-primary"
+          onClick={() => navigate(ROUTE_PAINTINGS)}
+        >
+          <ArrowBackIcon className="text-primary" />
+          <p>Home</p>
+        </button>
+      </div>
       <Background isDark={true} />
     </main>
   );
