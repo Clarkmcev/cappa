7f72e0b32ae682d1581ce1be3af54b72cefc37bc
Clark <mcevoyclark@gmail.com>
2024-10-07 19:24:47 +0200

    fix: docker compose setup

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 source .env
-echo "Building image : ${STUDIO_FRONTEND_IMAGE}"
-docker buildx build ./frontend --tag ${STUDIO_FRONTEND_IMAGE} --file ./frontend/Dockerfile
-echo "Success : Image ${STUDIO_FRONTEND_IMAGE} built!"
+# echo "Building image : ${STUDIO_FRONTEND_IMAGE}"
+# docker buildx build ./frontend --tag ${STUDIO_FRONTEND_IMAGE} --file ./frontend/Dockerfile
+# echo "Success : Image ${STUDIO_FRONTEND_IMAGE} built!"
+
+docker-compose build
