9242130e9e0b909e7a5fd07345aa10b3347fc946
Clark <mcevoyclark@gmail.com>
2024-10-14 19:45:45 +0200

    feat: first backend iteration

--- backend/main.go
+++ backend/main.go
@@ -1,47 +1,16 @@
 package main
 
 import (
-	"context"
-	"log"
-
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/s3"
-	"github.com/gin-gonic/gin"
+	"fmt"
+	"net/http"
 )
 
-func main() {
-		// Load the Shared AWS Configuration (~/.aws/config)
-		cfg, err := config.LoadDefaultConfig(context.TODO())
-		if err != nil {
-			log.Fatal(err)
-		}
-	
-		// Create an Amazon S3 service client
-		client := s3.NewFromConfig(cfg)
-	
-		// Get the first page of results for ListObjectsV2 for a bucket
-		output, err := client.ListObjectsV2(context.TODO(), &s3.ListObjectsV2Input{
-			Bucket: aws.String("cappa-bucket"),
-		})
-		if err != nil {
-			log.Fatal(err)
-		}
-	
-		log.Println("first page results:")
-		for _, object := range output.Contents {
-			log.Printf("key=%s size=%d", aws.ToString(object.Key), object.Size)
-		}
-
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, World!")
 }
 
-func getImages() {
-	
+func main() {
+    fmt.Println("Server is running on port 60")
+    http.HandleFunc("/", handler)
+    http.ListenAndServe(":60", nil)
 }
--- build/build.sh
+++ build/build.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
 source .env
-# echo "Building image : ${STUDIO_FRONTEND_IMAGE}"
-# docker buildx build ./frontend --tag ${STUDIO_FRONTEND_IMAGE} --file ./frontend/Dockerfile
-# echo "Success : Image ${STUDIO_FRONTEND_IMAGE} built!"
-
 docker-compose build
-# builds => cappa-caddy
