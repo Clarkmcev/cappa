aa49e8c939d422fff0b5779251fd62fb7dbcb789
Clark <mcevoyclark@gmail.com>
2025-05-16 15:35:38 +0200

    fix: cleanup

--- frontend/src/components/Navigation/SideBar.tsx
+++ frontend/src/components/Navigation/SideBar.tsx
@@ -77,12 +77,6 @@ function SideBar() {
               );
             })}
           </div>
-          {/* <div ref={scope}>
-            <div
-              ref={boxRef}
-              className="opacity-0 absolute left-34 duration-[400ms] transition-all w-2 h-4 rounded-full bg-fourth/20"
-            ></div>
-          </div> */}
           <Links />
         </div>
       </div>
